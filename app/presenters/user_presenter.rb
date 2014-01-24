3b58e6e78f437fccbd3659500c8f31b6c73f06d4
Brian Norton <brian.nort@gmail.com>
2014-01-23 16:21:29 -0800

    Adding the verified time on a presented user

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -1,3 +1,3 @@
 class UserPresenter < Presenter
-  allow :phone, :udid, :token, :locale, :version, :timezone, :bundle
+  allow :phone, :udid, :token, :locale, :version, :timezone, :bundle, :verified_at
 end
--- spec/user_presenter_spec.rb
+++ spec/user_presenter_spec.rb
@@ -4,7 +4,7 @@ describe UserPresenter do
   let(:model) { create(:user) }
   subject { described_class.new(model) }
 
-  let(:allowed) { [:phone, :udid, :token, :locale, :version, :timezone, :bundle] }
+  let(:allowed) { [:phone, :udid, :token, :locale, :version, :timezone, :bundle, :verified_at] }
 
   it_behaves_like 'a presenter'
 end
